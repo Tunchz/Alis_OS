2410efe8e706f5b8f2c855b1ec81c027a34d2475
Tunchz <46678653+Tunchz@users.noreply.github.com>
2019-07-14 16:56:47 +0700

    Update index.js

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -29,7 +29,9 @@ function callOnClick(e) {
     console.log(e)
 }
 //var socket = io();
-var socket = io.connect("https://public.os.alis.fund");
+
+var socket = io("https://public.os.alis.fund");
+//var socket = io.connect("https://public.os.alis.fund");
 socket.on("connect", function() {
     console.log("Connected to server!");
     var e = location.pathname.split("/").filter(e => "" !== e);
